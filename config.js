3c48f22e6f8674fffdbe1eeb1ccdf1db4dfed0a2
LSsithu <140079494+LSsithu@users.noreply.github.com>
2024-09-21 07:05:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put session id",
+SESSION_ID: process.env.SESSION_ID || "rTphRZaI#MfrOZX0KQM2FU9vjG094-07kLhuTJr9zOZvfiX_cna0",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/tCkmbMx/IMG-20240913-WA0041.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi QUEEN-SENUM Is Online Now 💻\n*💻 Owner* - Rocky \n\n*💻 Owner Number* -94704104383",
 SUDO_NB: process.env.SUDO_NB || "94704104383",
