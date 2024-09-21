a5dd683043d031a95378246cbb5eaa737cfb8106
LSsithu <140079494+LSsithu@users.noreply.github.com>
2024-09-21 07:07:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "rTphRZaI#MfrOZX0KQM2FU9vjG094-07kLhuTJr9zOZvfiX_cna0",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/tCkmbMx/IMG-20240913-WA0041.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi QUEEN-SENUM Is Online Now 💻\n*💻 Owner* - Rocky \n\n*💻 Owner Number* -94704104383",
-SUDO_NB: process.env.SUDO_NB || "94704104383",
+SUDO_NB: process.env.SUDO_NB || "94771665143",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE:"false"
